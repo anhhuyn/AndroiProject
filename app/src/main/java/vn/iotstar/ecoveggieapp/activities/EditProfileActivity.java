22fc60e8ac6bbfa61631feeaf105d106a5f00e0e
nguyenthianhhuyen03@gmail.com <nguyenthianhhuyen03@gmail.com>
2025-03-14 23:16:07 +0700

    add edit_profile.xml

---
+++ app/src/main/java/vn/iotstar/ecoveggieapp/activities/EditProfileActivity.java
@@ -0,0 +1,17 @@
+package vn.iotstar.ecoveggieapp.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.iotstar.ecoveggieapp.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
